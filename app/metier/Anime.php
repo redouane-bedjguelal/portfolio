ac10baa0524761d109101257cb0ee84b11142cef
adminlabo <adminlabo@192.168.228.125>
2016-11-16 14:54:26 +0100

    Ajout Anime.php

--- app/metier/Anime.php
+++ app/metier/Anime.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Anime extends Model
 {
-    //
+    //On déclare la table Anime
+    
+    protected $table = 'anime';
+    
+    protected $fillable = [
+        'numanime',
+        'numstudio',
+        'nomanime',
+        'saisonanime',
+        'anneeanime',
+        'nbepisodes',
+        'resume',
+        'estfini'
+    ];
+    
+    public function __construct(){
+        $this->numanime = 0;
+    }
 }
